39e858fe00df4b9ae7a4bc98fa81e1cbb1ceb814
Denis Lesnikov <102676229+Iteey@users.noreply.github.com>
2022-12-07 19:01:06 +0200

    front-end update
    +Added map space
    +added mobile version of navigation
    +Added search appearance

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 var lastClickFloor
 var lastClickCase
+var lastClickCaseMobile
 function changeToActiveFloor(CurrentTab) { 
     if ( lastClickFloor === undefined) {
         lastClickFloor = document.getElementById("default_floor");
@@ -12,7 +13,7 @@ function changeToActiveFloor(CurrentTab) {
     lastClickFloor=CurrentTab
 
   }
-  function changeToActiveCase(CurrentTabCase) { 
+  function changeToActiveCase(CurrentTabCase, buttonCode) { 
     if ( lastClickCase === undefined) {
         lastClickCase = document.getElementById("default_case");
         lastClickCase.classList.remove("active-right-tab");
@@ -20,7 +21,47 @@ function changeToActiveFloor(CurrentTab) {
     else {
         lastClickCase.classList.remove("active-right-tab");
     }
+    if (buttonCode == 'A'){
+        addActive = document.getElementById("default_mobile");
+        addActive.classList.add("active-right-tab");
+        removeActive = document.getElementById("secondary_mobile");
+        removeActive.classList.remove("active-right-tab");
+    }
+    else if (buttonCode == 'B'){
+        addActive = document.getElementById("secondary_mobile");
+        addActive.classList.add("active-right-tab");
+        removeActive = document.getElementById("default_mobile");
+        removeActive.classList.remove("active-right-tab");
+    }
     CurrentTabCase.classList.add("active-right-tab");
-    lastClickCase=CurrentTabCase
+    lastClickCase=CurrentTabCase;
+
+  }
+  function changeToActiveMobileTab(CurrentTabCaseMobile, buttonCode) { 
+    if ( lastClickCaseMobile === undefined) {
+        lastClickCaseMobile = document.getElementById("default_mobile");
+        lastClickCaseMobile.classList.remove("active-right-tab");
+     }
+    else {
+        lastClickCaseMobile.classList.remove("active-right-tab");
+    }
+    if (buttonCode == 'A'){
+        addActive = document.getElementById("default_case");
+        addActive.classList.add("active-right-tab");
+        removeActive = document.getElementById("secondary_case");
+        removeActive.classList.remove("active-right-tab");
+        removeActive = document.getElementById("secondary_mobile");
+        removeActive.classList.remove("active-right-tab");
+    }
+    else if (buttonCode == 'B'){
+        addActive = document.getElementById("secondary_case");
+        addActive.classList.add("active-right-tab");
+        removeActive = document.getElementById("default_case");
+        removeActive.classList.remove("active-right-tab");
+        removeActive = document.getElementById("default_mobile");
+        removeActive.classList.remove("active-right-tab");
+    }
+    CurrentTabCaseMobile.classList.add("active-right-tab");
+    lastClickCaseMobile=CurrentTabCaseMobile;
 
   }

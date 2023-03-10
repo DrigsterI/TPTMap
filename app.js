ac49b5f7024263cde27d73589d8d4618e2f19ee1
Gabriel Jõe <gabriel.j6e@gmail.com>
2023-03-10 13:50:13 +0200

    fix app.js merge

--- app.js
+++ app.js
@@ -28,15 +28,6 @@ app.get('/', (req, res) => {
 
 app.get('/:building/:floor', (req, res) => {
   if (!req.params.building.match(/^[a-zA-Z]$/)) {
-<<<<<<< HEAD
-    return res.redirect('/');
-  }
-
-  if (!req.params.floor.match(/^[0-9]+$/)) {
-    return res.redirect('/');
-  }
-  if (fs.existsSync(`/imgs/maps/${req.params.building}/${req.params.floor}.svg`)) {
-=======
     return res.redirect("/");
   }
 
@@ -44,7 +35,6 @@ app.get('/:building/:floor', (req, res) => {
     return res.redirect("/");
   }
   if (fs.existsSync(`public/imgs/maps/${req.params.building}/${req.params.floor}.svg`)) {
->>>>>>> 6a470574474b8b8db4dfdae852f57c29e40b0a20
     return res.render('index', { mapUrl: `/imgs/maps/${req.params.building}/${req.params.floor}.svg` });
   }
   return res.render('index');
@@ -53,7 +43,6 @@ app.get('/:building/:floor', (req, res) => {
 app.post('/:building/:floor', (req, res) => {
   if (req.body.search) {
     const search = req.body.search.toUpperCase();
-<<<<<<< HEAD
     const groupIdsUrl = 'https://tahvel.edu.ee/hois_back/timetables/group/14?lang=ET';
     const groupByName = {};
     const roomRegex = /^[a-zA-Z](?!000)[0-9]{3}$/;
@@ -185,157 +174,6 @@ app.post('/:building/:floor', (req, res) => {
     }).on('error', (err) => res.redirect(`?error=${err.message}`));
   } else {
     return res.redirect('?error=' + 'No search property');
-=======
-    const groupIdsUrl = "https://tahvel.edu.ee";
-    const groupByName = {};
-    let roomRegex = /^[a-zA-Z](?!000)[0-9]{3}$/;
-    if (search.match(roomRegex)) {
-      return res.redirect(`/${search[0]}/${search[1]}?room=${search}`);
-    }
-    else {
-      const searchedGroupId = search;
-      https.get(groupIdsUrl, (response) => {
-        let data = '';
-
-        response.on('data', (chunk) => {
-          data += chunk;
-        });
-
-        response.on('end', () => {
-          try {
-            const responseObject = JSON.parse(data);
-            const content = responseObject.content;
-
-            content.forEach((group) => {
-              const groupId = group.id;
-              const groupNameEt = group.nameEt;
-              const groupNameEn = group.nameEn;
-
-              groupByName[groupNameEt] = groupId;
-              if (!groupByName[groupNameEn]) {
-                groupByName[groupNameEn] = groupId;
-              }
-            });
-            if (groupByName[searchedGroupId]) {
-              let date = new Date();
-              date.setHours(0, 0, 0, 0);
-
-              let dateToday = date.toISOString();
-              date.setDate(date.getDate() + 7);
-
-              let dateInWeek = date.toISOString();
-
-              const timetableUrl = `https://tahvel.edu.ee/hois_back/timetableevents/timetableByGroup/14?from=${dateToday}&studentGroups=${groupByName[searchedGroupId]}&thru=${dateInWeek}`;
-
-              https.get(timetableUrl, (response) => {
-                let data = '';
-
-                // A chunk of data has been received.
-                response.on('data', (chunk) => {
-                  data += chunk;
-                });
-
-                // The whole response has been received. Print out the result.
-                response.on('end', () => {
-                  try {
-                    const responseObject = JSON.parse(data);
-                    const timetableEvents = responseObject.timetableEvents;
-
-                    const roomsByDay = {};
-
-                    timetableEvents.forEach((event) => {
-                      if (event.rooms && event.rooms.length > 0) {
-                        const timeStart = event.timeStart;
-                        const timeEnd = event.timeEnd;
-                        const roomCode = event.rooms[0].roomCode;
-                        const date = event.date;
-
-                        if (!roomsByDay[date]) {
-                          roomsByDay[date] = {};
-                        }
-
-                        if (!roomsByDay[date][timeStart]) {
-                          roomsByDay[date][timeStart] = [];
-                        }
-
-                        roomsByDay[date][timeStart].push(roomCode);
-                      }
-                    });
-
-                    Object.keys(roomsByDay).forEach((date) => {
-                      const dateObject = roomsByDay[date];
-                      const sortedTimes = Object.keys(dateObject).sort();
-                      const sortedDateObject = {};
-
-                      sortedTimes.forEach((time) => {
-                        sortedDateObject[time] = dateObject[time];
-                      });
-
-                      roomsByDay[date] = sortedDateObject;
-                    });
-
-                    const sortedDates = Object.keys(roomsByDay).sort();
-                    const sortedRoomsByDay = {};
-
-                    sortedDates.forEach((date) => {
-                      sortedRoomsByDay[date] = roomsByDay[date];
-                    });
-
-                    let dayData = [];
-                    if (sortedRoomsByDay[dateToday]) {
-                      dayData = sortedRoomsByDay[dateToday];
-                    }
-                    else {
-                      dayData = Object.values(sortedRoomsByDay)[0];
-                    }
-
-                    let nextRoom;
-                    for (let time in dayData) {
-                      let timeParts = time.split(":");
-                      let hours = parseInt(timeParts[0], 10);
-                      let minutes = parseInt(timeParts[1], 10);
-
-                      let timeIndex = Object.keys(dayData).indexOf(time);
-                      if (date.getHours() === hours && Math.abs(date.getMinutes() - minutes) <= 15) {
-                        nextRoom = dayData[time][0];
-                        return res.redirect(`/${nextRoom[0]}/${nextRoom[1]}?room=${nextRoom}`);
-                      }
-                      else if (date.getHours() < hours && date.getMinutes() === minutes) {
-                        if (timeIndex < Object.keys(dayData).length - 1) {
-                          nextRoom = Object.values(dayData)[timeIndex + 1][0];
-                          return res.redirect(`/${nextRoom[0]}/${nextRoom[1]}?room=${nextRoom}`);
-                        }
-                      }
-                    }
-
-                    if (nextRoom == null) {
-                      return res.redirect("?error=" + "No next rooms");
-                    }
-                  } catch (error) {
-                    console.log("Search error: " + error);
-                    return res.redirect("?error=" + "Server error");
-                  }
-                });
-              }).on("error", (err) => {
-                return res.redirect("?error=" + err.message);
-              });
-            }
-            else {
-              return res.redirect("?error=" + "Group not found");
-            }
-          } catch (error) {
-            console.log("Search error: " + error);
-            return res.redirect("?error=" + "Server error");
-          }
-        });
-      }).on("error", (err) => {
-        return res.redirect("?error=" + err.message);
-      });
-    }
-  }
-  else {
-    return res.redirect("?error=" + "No search property");
->>>>>>> 6a470574474b8b8db4dfdae852f57c29e40b0a20
   }
 });
 
@@ -345,8 +183,4 @@ app.get('/about', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}/`);
-<<<<<<< HEAD
 });
-=======
-})
->>>>>>> 6a470574474b8b8db4dfdae852f57c29e40b0a20
